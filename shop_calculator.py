c07ecb612ee304f13de7bd6f6c4101195205e873
debbyjohn22 <deborahjohnladi@gmail.com>
2026-09-14 23:15:19 +0100

    shop_calculator.py

---
+++ shop_calculator.py
@@ -0,0 +1,8 @@
+item1 = int(input("Enter price: "))
+item2 = int(input("Enter price: "))
+item3 = int(input("Enter price: "))
+
+total = item1 + item2 + item3
+customer_paid = 10000
+customer_change = customer_paid - total
+print(customer_change) 
