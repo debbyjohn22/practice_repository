13d531c230edc12e74437a1a88adc0d2ed7dbc9b
debbyjohn22 <deborahjohnladi@gmail.com>
2026-09-18 14:22:18 +0100

    .

---
+++ grading_system.py
@@ -0,0 +1,22 @@
+score = 65
+if score >= 70:
+    print("A")
+elif score >= 60:
+    print("B")
+elif score >= 50:
+    print("C")
+else:
+    print("fail")
+
+print()   
+score = 65
+if score >= 70:
+    print("A")
+elif score >=60:
+    print("B")
+elif score >= 50:
+    print("C")
+elif score >= 40:
+    print("D") 
+else:
+    print("F")
---
+++ studeny_score.py
@@ -0,0 +1,11 @@
+score = int(input("enter score: "))
+if score >= 70:
+    print("A")
+elif score >= 60:
+    print("B") 
+elif score >= 50:
+    print("C") 
+elif score >= 40:
+    print("D")
+else:
+    print("F")             
