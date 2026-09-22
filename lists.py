dcdf4d6be41faa63b03efd6fee0bd14588d545e7
debbyjohn22 <deborahjohnladi@gmail.com>
2026-09-22 16:58:30 +0100

    lists.py

---
+++ lists.py
@@ -0,0 +1,8 @@
+ai_tools = ["python", "Variables",  "Data types"]
+ai_tools.append("working program")
+print(ai_tools)
+print(ai_tools[0])
+print(len(ai_tools))
+print(ai_tools[3])
+for i in ai_tools:
+    print(i)
