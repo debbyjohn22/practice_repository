317f1da6669ef8957748aa83002fdac40667c65b
debbyjohn22 <deborahjohnladi@gmail.com>
2026-09-23 11:06:22 +0100

    lists.py

--- lists.py
+++ lists.py
@@ -6,3 +6,9 @@ print(len(ai_tools))
 print(ai_tools[3])
 for i in ai_tools:
     print(i)
+
+for tool in ai_tools:
+    if tool == "cooking":
+        print(f"{tool} is your first language!")
+    else:
+        print(f"Also learning {tool}")
